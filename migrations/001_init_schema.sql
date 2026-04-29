d5ec5af7c50090d684d6cf7e9f7c7d32e6d85dc6
athallahdx <athallahgamer4@gmail.com>
2026-04-30 01:11:13 +0700

    feat: nullable profile image

--- migrations/001_init_schema.sql
+++ migrations/001_init_schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(20),
-    profile VARCHAR(255),
+    profile VARCHAR(255) NULL DEFAULT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user', 'promotor') DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
